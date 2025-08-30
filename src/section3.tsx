e1bb9aa56ffa138d5e0e78192899b58c82f377f6
guhang-web <dacjj123@naver.com>
2025-08-31 01:27:51 +0900

    section3 클릭 이벤트 수정중

--- src/section3.tsx
+++ src/section3.tsx
@@ -1,49 +1,38 @@
 // src/section3.tsx
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Mousewheel } from 'swiper/modules'
-import './section3.css'
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel } from 'swiper/modules';
+import './section3.css';
 
 type LayoutVars = {
-  // 좌측 묶음
   leftTop?: number; leftLeft?: number; leftWidth?: number; leftGap?: number;
-  // 가운데 메인
   centerW?: number; centerH?: number;
-  // VIEW MORE 원형
   vpTop?: number; vpRight?: number; vpSize?: number;
-  // 우측 프리뷰 묶음
   rightBottom?: number; rightRight?: number; rightWidth?: number;
   disneyTop?: number; dLogoTop?: number; dLogoLeft?: number; dLogoSize?: number;
-  logoW?: number;        // 좌측 로고 너비
-  textFS?: number;       // 좌측 텍스트 글자 크기
-  textLH?: number;       // 좌측 텍스트 줄간격
-  textMaxW?: number;     // 좌측 텍스트 최대 너비
-  centerX?: number;      // 메인 이미지 X 보정(px)
-  centerY?: number;      // 메인 이미지 Y 보정(px)
-  logoTop?: number;   // 좌측 로고 top(px)
-  logoLeft?: number;  // 좌측 로고 left(px)
-  textTop?: number;   // 좌측 문단 top(px)
-  textLeft?: number;  // 좌측 문단 left(px)
-
-}
+  logoW?: number; textFS?: number; textLH?: number; textMaxW?: number;
+  logoTop?: number; logoLeft?: number; textTop?: number; textLeft?: number;
+  centerMaxW?: number; centerMaxH?: number; centerX?: number; centerY?: number; centerScale?: number;
+  rightImgW?: number; rightImgH?: number; rightImgTop?: number; rightImgLeft?: number;
+  ghostOpacity?: number; ghostGray?: number;
+};
 
 type SlideData = {
-  logo: string
-  text: string
-  image: string
-  rightLogo?: string
-  rightGhost?: string           // 마지막 슬라이드는 undefined로 두면 우측 흑백 안 나옴
-  layout?: LayoutVars           // ← 이 숫자만 바꾸면 슬라이드별 배치가 바뀜
-}
+  logo: string;
+  text: string;
+  image: string;
+  rightLogo?: string;
+  rightGhost?: string;
+  layout?: LayoutVars;
+};
 
-/** ✅ 여기 배열만 수정(이미지·텍스트·숫자) */
 const baseSlides = [
   {
     logo: '/section3Img/marvelLogo.png',
     text: '마블 팬이라면 누구나 마음 속에 자신만의 영웅 이야기가 있기 마련이죠.<br/>자신만의 세계를 만들고 마음 속의 이야기를 한껏 펼쳐보세요!',
     image: '/section3Img/marvelImg.png',
     rightLogo: '/section3Img/disneyLogo1.png',
-    // layout: { leftTop: 220, centerW: 570, vpRight: -20 },
+    layout: { leftTop: 220, centerY: -6, textTop: 160, rightImgW: 340, rightImgH: 484, rightImgTop: 70, rightImgLeft: 0 },
   },
   {
     logo: '/section3Img/disneyLogo2.png',
@@ -51,126 +40,213 @@ const baseSlides = [
     image: '/section3Img/disneyImg.png',
     rightLogo: '/section3Img/ninjagoLogo1.png',
     layout: {
-      /* 좌측(로고+설명) */
-      leftTop: 160,      // 좌측 묶음의 위쪽 위치
-      leftLeft: 140,     // 좌측 묶음의 왼쪽 위치
-      leftWidth: 460,    // 좌측 묶음의 폭
-      leftGap: 20,       // 로고와 문단 사이 간격
-
-      /* 가운데(메인 이미지) */
-      centerW: 540,      // 메인 이미지 박스 폭
-      centerH: 700,      // 메인 이미지 박스 높이
-      /* (아래 두 값은 '미세 위치 보정' – 선택) */
-      centerX: -30,      // 왼쪽(-) / 오른쪽(+)으로 n px 이동
-      centerY: 0,        // 위(-) / 아래(+)로 n px 이동
-
-      /* VIEW MORE (이벤트 나중이지만 위치는 대략) */
-      vpTop: 480,
-      vpRight: 330,      // 값이 클수록 원이 왼쪽으로 이동
-      vpSize: 180,
-
-      /* 우측(흑백 프리뷰 묶음) */
-      rightBottom: 80,
-      rightRight: 120,
-      rightWidth: 380,
-
-      /* 우측 작은 로고 위치/크기 */
-      disneyTop: 110,
-      dLogoTop: 220,
-      dLogoLeft: -20,
-      dLogoSize: 120,
+      leftTop: 160, leftLeft: 200, leftWidth: 460, leftGap: 20, textTop: 220,
+      centerW: 548, centerH: 780, centerX: -30, centerY: 0,
+      vpTop: 620, vpRight: 440, vpSize: 200,
+      rightBottom: 80, rightRight: 120, rightWidth: 413,
+      rightImgW: 413, rightImgH: 413, rightImgTop: 130, rightImgLeft: 0,
+      disneyTop: 110, dLogoTop: 220, dLogoLeft: -50, dLogoSize: 120,
     },
   },
   {
     logo: '/section3Img/ninjagoLogo2.png',
-    text: '수많은 이야기와 낭자가 오가는 닌자고 시티의 중심,<br/>모험은 이곳에서 시작됩니다.',
+    text: '수많은 이야기와 닌자가 오가는 닌자고 시티의 중심,<br/>모험은 이곳에서 시작됩니다.',
     image: '/section3Img/ninjagoImg.png',
     rightLogo: '/section3Img/starwarsLogo1.png',
-    // layout: { vpTop: 280 },
+    layout: {
+      leftTop: 230, leftLeft: 190, leftWidth: 460, textTop: 120,
+      centerW: 700, centerH: 700, centerX: -100, centerY: 20,
+      vpTop: 200, vpRight: 0, vpSize: 200,
+      rightBottom: 80, rightRight: 40, rightWidth: 500,
+      rightImgW: 442, rightImgH: 372, rightImgTop: 210, rightImgLeft: 0,
+      disneyTop: 160, dLogoTop: 120, dLogoLeft: -10, dLogoSize: 120,
+    },
   },
   {
     logo: '/section3Img/starwarsLogo2.png',
     text: '세월을 넘어 사랑받아온 은하계 밀레니엄 팔콘,<br/>최고의 세팅과 압도적 디테일을 지금 만나보세요.',
     image: '/section3Img/starwarsImg.png',
     rightLogo: '/section3Img/cityLogo1.png',
+    layout: {
+      leftTop: 180, leftLeft: 200, leftWidth: 460, textTop: 160,
+      centerW: 813, centerH: 686, centerX: 20, centerY: 170,
+      vpTop: 460, vpRight: 85, vpSize: 200,
+      rightBottom: 100, rightRight: -20, rightWidth: 500,
+      rightImgW: 451, rightImgH: 338, rightImgTop: 260, rightImgLeft: -40,
+      disneyTop: 20, dLogoTop: 280, dLogoLeft: -20, dLogoSize: 131,
+    },
   },
   {
     logo: '/section3Img/cityLogo2.png',
     text: '거대한 임무를 가르고 나아가는 북극 탐사선,<br/>신비한 대자연 속에서 모험이 펼쳐집니다.',
     image: '/section3Img/cityImg.png',
     rightLogo: '/section3Img/harryPotterLogo1.png',
+    layout: {
+      leftTop: 150, leftLeft: 140, leftWidth: 460, logoLeft: 0, textTop: 160, textLeft: 80,
+      centerW: 775, centerH: 582, centerX: 0, centerY: 30,
+      vpTop: 450, vpRight: 580, vpSize: 200,
+      rightBottom: 130, rightRight: -20, rightWidth: 500,
+      rightImgW: 444, rightImgH: 389, rightImgTop: 260, rightImgLeft: -10,
+      disneyTop: 10, dLogoTop: 320, dLogoLeft: 50, dLogoSize: 131,
+    },
   },
-  // ✅ 마지막(우측 흑백/로고 없음)
   {
     logo: '/section3Img/harryPotterLogo2.png',
-    text: '수많은 마법사의 꿈이 머물렀던 곳, 호그와트.<br/>신비로운 마법의 세계로 초대합니다.',
+    text: '수많은 마법사의 꿈이 머물렀던 곳, 호그와트.<br/>신비로운 마법의 세계로 모험을 떠나보세요.',
     image: '/section3Img/harryPotterImg.png',
-    // layout: { rightWidth: 0 } // 필요 시 오른쪽 묶음 자체가 없어도 됨(아래 조건부 렌더링)
+    layout: {
+      leftTop: 180, leftLeft: 160, leftWidth: 460, logoLeft: -20, textTop: 140, textLeft: 80,
+      centerW: 760, centerH: 665, centerX: 0, centerY: 0,
+      vpTop: 402, vpRight: -125, vpSize: 200,
+      rightBottom: 120, rightRight: 80,
+    },
   },
-] as const
+] as const;
 
-// 다음 장 이미지를 자동으로 우측 흑백 프리뷰에 넣기(마지막은 undefined)
 const slides: SlideData[] = baseSlides.map((s, i, arr) => ({
   ...s,
   rightGhost: i < arr.length - 1 ? arr[i + 1].image : undefined,
-}))
-
-// CSS 변수 주입 유틸(숫자를 px로 바꿔서 style에 넣어줌)
+}));
 
 type CSSVarStyle = React.CSSProperties & Record<`--${string}`, string | number>;
-
 const toVars = (v?: LayoutVars): CSSVarStyle => {
   const px = (n?: number) => (n === undefined ? undefined : `${n}px`);
   return {
-    // === 기존 매핑 ===
-    ...(v?.leftTop     !== undefined && { ['--left-top']:      px(v.leftTop) }),
-    ...(v?.leftLeft    !== undefined && { ['--left-left']:     px(v.leftLeft) }),
-    ...(v?.leftWidth   !== undefined && { ['--left-width']:    px(v.leftWidth) }),
-    ...(v?.leftGap     !== undefined && { ['--left-gap']:      px(v.leftGap) }),
-    ...(v?.centerW     !== undefined && { ['--center-w']:      px(v.centerW) }),
-    ...(v?.centerH     !== undefined && { ['--center-h']:      px(v.centerH) }),
-    ...(v?.centerX     !== undefined && { ['--center-x']:      px(v.centerX) }),
-    ...(v?.centerY     !== undefined && { ['--center-y']:      px(v.centerY) }),
-    ...(v?.vpTop       !== undefined && { ['--vp-top']:        px(v.vpTop) }),
-    ...(v?.vpRight     !== undefined && { ['--vp-right']:      px(v.vpRight) }),
-    ...(v?.vpSize      !== undefined && { ['--vp-size']:       px(v.vpSize) }),
-    ...(v?.rightBottom !== undefined && { ['--right-bottom']:  px(v.rightBottom) }),
-    ...(v?.rightRight  !== undefined && { ['--right-right']:   px(v.rightRight) }),
-    ...(v?.rightWidth  !== undefined && { ['--right-width']:   px(v.rightWidth) }),
-    ...(v?.disneyTop   !== undefined && { ['--disney-top']:    px(v.disneyTop) }),
-    ...(v?.dLogoTop    !== undefined && { ['--dlogo-top']:     px(v.dLogoTop) }),
-    ...(v?.dLogoLeft   !== undefined && { ['--dlogo-left']:    px(v.dLogoLeft) }),
-    ...(v?.dLogoSize   !== undefined && { ['--dlogo-size']:    px(v.dLogoSize) }),
-
-    // === 새로 추가한 개별 위치/타이포 매핑 ===
-    ...(v?.logoTop     !== undefined && { ['--left-logo-top']:  px(v.logoTop) }),
-    ...(v?.logoLeft    !== undefined && { ['--left-logo-left']: px(v.logoLeft) }),
-    ...(v?.logoW       !== undefined && { ['--logo-w']:        px(v.logoW) }),
-    ...(v?.textTop     !== undefined && { ['--left-text-top']:  px(v.textTop) }),
-    ...(v?.textLeft    !== undefined && { ['--left-text-left']: px(v.textLeft) }),
-    ...(v?.textFS      !== undefined && { ['--text-fs']:        px(v.textFS) }),
-    ...(v?.textLH      !== undefined && { ['--text-lh']:        px(v.textLH) }),
-    ...(v?.textMaxW    !== undefined && { ['--text-mw']:        px(v.textMaxW) }),
+    ...(v?.leftTop !== undefined && { ['--left-top']: px(v.leftTop) }),
+    ...(v?.leftLeft !== undefined && { ['--left-left']: px(v.leftLeft) }),
+    ...(v?.leftWidth !== undefined && { ['--left-width']: px(v.leftWidth) }),
+    ...(v?.leftGap !== undefined && { ['--left-gap']: px(v.leftGap) }),
+    ...(v?.centerMaxW !== undefined && { ['--center-max-w']: px(v.centerMaxW) }),
+    ...(v?.centerMaxH !== undefined && { ['--center-max-h']: px(v.centerMaxH) }),
+    ...(v?.centerW !== undefined && { ['--center-w']: px(v.centerW) }),
+    ...(v?.centerH !== undefined && { ['--center-h']: px(v.centerH) }),
+    ...(v?.centerX !== undefined && { ['--center-x']: px(v.centerX) }),
+    ...(v?.centerY !== undefined && { ['--center-y']: px(v.centerY) }),
+    ...(v?.centerScale !== undefined && { ['--center-scale']: v.centerScale }),
+    ...(v?.vpTop !== undefined && { ['--vp-top']: px(v.vpTop) }),
+    ...(v?.vpRight !== undefined && { ['--vp-right']: px(v.vpRight) }),
+    ...(v?.vpSize !== undefined && { ['--vp-size']: px(v.vpSize) }),
+    ...(v?.rightBottom !== undefined && { ['--right-bottom']: px(v.rightBottom) }),
+    ...(v?.rightRight !== undefined && { ['--right-right']: px(v.rightRight) }),
+    ...(v?.rightWidth !== undefined && { ['--right-width']: px(v.rightWidth) }),
+    ...(v?.disneyTop !== undefined && { ['--disney-top']: px(v.disneyTop) }),
+    ...(v?.dLogoTop !== undefined && { ['--dlogo-top']: px(v.dLogoTop) }),
+    ...(v?.dLogoLeft !== undefined && { ['--dlogo-left']: px(v.dLogoLeft) }),
+    ...(v?.dLogoSize !== undefined && { ['--dlogo-size']: px(v.dLogoSize) }),
+    ...(v?.logoTop !== undefined && { ['--left-logo-top']: px(v.logoTop) }),
+    ...(v?.logoLeft !== undefined && { ['--left-logo-left']: px(v.logoLeft) }),
+    ...(v?.logoW !== undefined && { ['--logo-w']: px(v.logoW) }),
+    ...(v?.textTop !== undefined && { ['--left-text-top']: px(v.textTop) }),
+    ...(v?.textLeft !== undefined && { ['--left-text-left']: px(v.textLeft) }),
+    ...(v?.textFS !== undefined && { ['--text-fs']: px(v.textFS) }),
+    ...(v?.textLH !== undefined && { ['--text-lh']: px(v.textLH) }),
+    ...(v?.textMaxW !== undefined && { ['--text-mw']: px(v.textMaxW) }),
+    ...(v?.rightImgW !== undefined && { ['--right-img-w']: px(v.rightImgW) }),
+    ...(v?.rightImgH !== undefined && { ['--right-img-h']: px(v.rightImgH) }),
+    ...(v?.rightImgTop !== undefined && { ['--right-img-top']: px(v.rightImgTop) }),
+    ...(v?.rightImgLeft !== undefined && { ['--right-img-left']: px(v.rightImgLeft) }),
+    ...(v?.ghostOpacity !== undefined && { ['--ghost-opacity']: v.ghostOpacity }),
+    ...(v?.ghostGray !== undefined && { ['--ghost-gray']: `${v.ghostGray}%` }),
   };
 };
 
-/* ── 슬라이드 UI ── */
-function Section3Slide({ logo, text, image, rightLogo, rightGhost, layout }: SlideData) {
+function Section3Slide({
+  logo, text, image, rightLogo, rightGhost, layout, index, swiper,
+}: SlideData & { index: number; swiper: any }) {
+
+  const meddleRef = useRef<HTMLUListElement>(null);
+  const centralImgRef = useRef<HTMLImageElement>(null);  // 중앙 이미지에 대한 ref 추가
+  const [cursor, setCursor] = useState({ x: 0, y: 0 });
+  const [active, setActive] = useState(false);
+  // 마우스 이동 처리
+  const onMove: React.MouseEventHandler<HTMLUListElement> = (e) => {
+    const wrap = meddleRef.current;
+    if (!wrap) return;
+    const rect = wrap.getBoundingClientRect();
+    setCursor({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+  };
+
+  // 우측 흑백 이미지 클릭 → 중앙으로 비행 → 슬라이드 이동
+  const onGhostClick = (imgEl: HTMLImageElement) => {
+    if (!imgEl || !meddleRef.current || !swiper) return;
+    
+
+    // 목표: 가운데 메인 컨테이너의 정중앙
+    const target = meddleRef.current.getBoundingClientRect();
+    const imgRect = imgEl.getBoundingClientRect();
+
+    const dx = (target.left + target.width / 2) - (imgRect.left + imgRect.width / 2);
+    const dy = (target.top + target.height / 2) - (imgRect.top + imgRect.height / 2);
+
+    const scale = Math.min(target.width / imgRect.width, target.height / imgRect.height) * 0.9;
+
+    // 비행 애니메이션 효과
+    imgEl.style.setProperty('--fly-x', `${Math.round(dx)}px`);
+    imgEl.style.setProperty('--fly-y', `${Math.round(dy)}px`);
+    imgEl.style.setProperty('--fly-scale', `${scale.toFixed(3)}`);
+    imgEl.classList.add('is-flying');
+
+    // **중앙 이미지(마블 이미지) opacity를 0으로 설정해서 사라지게 처리**
+    const centralImg = document.querySelector('.marvelImg') as HTMLImageElement;
+    if (centralImg) {
+      centralImg.style.opacity = '0'; // 중앙 이미지 사라짐
+    }
+
+
+    // 휠 비활성화 (슬라이드 전환 중)
+    swiper.mousewheel?.disable?.();
+
+    // 애니메이션 끝나면 슬라이드 전환
+    const flightMs = 500;
+    imgEl.addEventListener('transitionend', () => {
+      imgEl.style.opacity = '1'; // 이동 후 투명도 복원
+      swiper.slideTo(index + 1, 700); // 다음 슬라이드로 이동
+      // 휠 다시 활성화
+      setTimeout(() => swiper.mousewheel?.enable?.(), 750);
+
+      // 상태 리셋 (애니메이션 끝난 후)
+      setTimeout(() => imgEl.classList.remove('is-flying'), 50);
+      // 비행 끝나고 중앙 이미지를 다시 보이게 설정
+      setTimeout(() => {
+        if (centralImg) {
+          centralImg.style.opacity = '1'; // 중앙 이미지 보이게
+          imgEl.style.opacity = '0.5';
+        }
+      }, 500); // 500ms 후에 중앙 이미지 복원
+    }, { once: true });
+  };
+
   return (
     <div className="s3-slide" style={toVars(layout)}>
       {/* 좌측 */}
-      <ul className="section3Left">
-        <img className="marvelLogo" src={logo} alt="브랜드 로고" />
-        <p dangerouslySetInnerHTML={{ __html: text }} />
+      <ul className="section3Left" style={toVars(layout)}>
+        <img
+          className={`marvelLogo ${(layout?.logoTop !== undefined || layout?.logoLeft !== undefined) ? 'abs' : ''}`}
+          src={logo}
+          alt="브랜드 로고"
+        />
+        <p
+          className={`leftText ${(layout?.textTop !== undefined || layout?.textLeft !== undefined) ? 'abs' : ''}`}
+          dangerouslySetInnerHTML={{ __html: text }}
+        />
       </ul>
 
       {/* 가운데 */}
-      <ul className="section3Meddle">
+      <ul
+        ref={meddleRef}
+        className={`section3Meddle ${active ? 'cursor-mode' : ''}`}
+        style={
+          { ['--cursor-x']: `${cursor.x}px`, ['--cursor-y']: `${cursor.y}px` } as CSSVarStyle
+        }
+        onMouseEnter={() => setActive(true)}
+        onMouseLeave={() => setActive(false)}
+        onMouseMove={onMove}
+      >
         <img className="marvelImg" src={image} alt="메인 이미지" />
         <li className="viewPoint"><p>VIEW MORE</p></li>
       </ul>
 
-      {/* 우측(로고/흑백 둘 중 하나라도 있을 때만 표시) */}
+      {/* 우측(프리뷰) */}
       <div className="section3Right">
         <ul className="scroll-hint">
           <li><span className="mouse"></span></li>
@@ -178,47 +254,49 @@ function Section3Slide({ logo, text, image, rightLogo, rightGhost, layout }: Sli
         </ul>
 
         {(rightLogo || rightGhost) && (
-          <ul className="disneyPoint">
+          <ul className="disneyPoint" style={toVars(layout)}>
             {rightLogo && (
               <li className="disneyLogo1"><img src={rightLogo} alt="우측 로고" /></li>
             )}
             {rightGhost && (
-              <li className="disneyBlock"><img src={rightGhost} alt="다음 장 프리뷰" /></li>
+              <li className="disneyBlock">
+                <img src={rightGhost} alt="다음 장 프리뷰" onClick={(e) => onGhostClick(e.currentTarget)} />
+              </li>
             )}
           </ul>
         )}
       </div>
     </div>
-  )
+  );
 }
 
 export default function Section3() {
+  const [swiper, setSwiper] = useState<any>(null);
+
   return (
     <section id="section3">
       <Swiper
         className="s3-swiper"
         modules={[Mousewheel]}
+        onSwiper={setSwiper}  // swiper 인스턴스를 상태로 설정
         slidesPerView={1}
         spaceBetween={0}
-        loop={false}              // 마지막에서 처음으로 안 돌아감
+        loop={false}
         speed={700}
         mousewheel={{
           forceToAxis: false,
-          releaseOnEdges: true,   // 끝에서 아래 섹션으로 스크롤 이어짐
+          releaseOnEdges: true,
           sensitivity: 1,
           thresholdDelta: 10,
         }}
         allowTouchMove={false}
-        onReachEnd={() =>
-          document.getElementById('section4')?.scrollIntoView({ behavior: 'smooth' })
-        }
       >
         {slides.map((s, i) => (
           <SwiperSlide key={i}>
-            <Section3Slide {...s} />
+            <Section3Slide {...s} index={i} swiper={swiper} />
           </SwiperSlide>
         ))}
       </Swiper>
     </section>
-  )
+  );
 }
--- src/section4.tsx
+++ src/section4.tsx
@@ -12,8 +12,8 @@ const Section4 = () => {
                 <div id="section4Main">
                     <div className="section4Up">
                         <div className="col col-left">
-                            <img src="/section4Img/lego1.png" alt="레고 세트 1" />
-                            <img src="/section4Img/lego2.png" alt="레고 세트 2" />
+                            <img className="fall piece-1" src="/section4Img/lego1.png" alt="레고 세트 1" />
+                            <img className="fall piece-2" src="/section4Img/lego2.png" alt="레고 세트 2" />
                         </div>
 
                         <div className="col col-center">
@@ -21,38 +21,65 @@ const Section4 = () => {
                             <h2>나이에 딱 맞는 LEGO</h2>
                             <p>
                                 나이에 따라 달라지는 관심과 호기심, 레고는 그 모든 순간에 함께합니다.<br />
-                                즐겁게 놀고, 배우고, 성장하는 완벽한 선택
+                                즐겁게 놀고, 배우고, 성장하는 완벽한 선택!
                             </p>
                         </div>
 
                         <div className="col col-right">
-                            <img src="/section4Img/lego3.png" alt="레고 세트 3" />
-                            <img src="/section4Img/lego4.png" alt="레고 세트 4" />
+                            <img className="fall piece-3" src="/section4Img/lego3.png" alt="레고 세트 3" />
+                            <img className="fall piece-4" src="/section4Img/lego4.png" alt="레고 세트 4" />
                         </div>
                     </div>
 
                     <div className='section4Meddle'>
                         <ul className='greenBox'>
+                            <li><img className='boxHead' src="/section4Img/greenLego1.png" alt="초록레고 머리" /></li>
+                            <li><img className='boxBody' src="/section4Img/greenLego2.png" alt="초록레고 몸통" /></li>
+                            <li><img className='darkEffet' src="/section4Img/darkEffet.png" alt="그림자효과" /></li>
+                            <li><img className='boxImg' src="/section4Img/greenImg.png" alt="초록레고 이미지" /></li>
+                            <li className='boxPage'><h3>유아</h3>
+                                <p>브릭을 처음 만나는 유아들을 위한<br />안전하고 즐거운 조립</p></li>
                         </ul>
-                        <ul className='blueBox'></ul>
-                        <ul className='redBox'></ul>
-                        <ul className='yellowBox'></ul>
+                        <ul className='blueBox'>
+                            <li><img className='boxHead' src="/section4Img/blueLego1.png" alt="초록레고 머리" /></li>
+                            <li><img className='boxBody' src="/section4Img/blueLego2.png" alt="초록레고 몸통" /></li>
+                            <li><img className='darkEffet' src="/section4Img/darkEffet.png" alt="그림자효과" /></li>
+                            <li><img className='boxImg' src="/section4Img/blueImg.png" alt="초록레고 이미지" /></li>
+                            <li className='boxPage'><h3>어린이</h3>
+                                <p>창의력과 몰입을 키우는<br />테마 중심놀이</p></li>
+                        </ul>
+                        <ul className='redBox'>
+                            <li><img className='boxHead' src="/section4Img/redLego1.png" alt="초록레고 머리" /></li>
+                            <li><img className='boxBody' src="/section4Img/redLego2.png" alt="초록레고 몸통" /></li>
+                            <li><img className='darkEffet' src="/section4Img/darkEffet.png" alt="그림자효과" /></li>
+                            <li><img className='boxImg' src="/section4Img/redImg.png" alt="초록레고 이미지" /></li>
+                            <li className='boxPage'><h3>청소년</h3>
+                                <p>구조와 기능을 탐구하며<br />성장하는 조립 경험</p></li>
+                        </ul>
+                        <ul className='yellowBox'>
+                            <li><img className='boxHead' src="/section4Img/yellowLego1.png" alt="초록레고 머리" /></li>
+                            <li><img className='boxBody' src="/section4Img/yellowLego2.png" alt="초록레고 몸통" /></li>
+                            <li><img className='darkEffet' src="/section4Img/darkEffet.png" alt="그림자효과" /></li>
+                            <li><img className='boxImg' src="/section4Img/yellowImg.png" alt="초록레고 이미지" /></li>
+                            <li className='boxPage'><h3>성인</h3>
+                                <p>고급 모델과 감성적인<br />수집용 시리즈</p></li>
+                        </ul>
                     </div>
                     <div className='section4Down'>
                         <div className='section4LeftDown'>
-                            <img src="/section4Img/lego5.png" alt="레고 세트 5" />
-                            <img src="/section4Img/lego6.png" alt="레고 세트 6" />
-                            <img src="/section4Img/lego7.png" alt="레고 세트 7" />
-                            <img src="/section4Img/lego8.png" alt="레고 세트 8" />
+                            <img className="fall piece-5" src="/section4Img/lego5.png" alt="레고 세트 5" />
+                            <img className="fall piece-6" src="/section4Img/lego6.png" alt="레고 세트 6" />
+                            <img className="fall piece-7" src="/section4Img/lego7.png" alt="레고 세트 7" />
+                            <img className="fall piece-8" src="/section4Img/lego8.png" alt="레고 세트 8" />
                         </div>
                         <div className='section4RightDown'>
-                            <img src="/section4Img/lego9.png" alt="레고 세트 9" />
-                            <img src="/section4Img/lego10.png" alt="레고 세트 10" />
-                            <img src="/section4Img/lego11.png" alt="레고 세트 11" />
-                            <img src="/section4Img/lego12.png" alt="레고 세트 12" />
-                            <img src="/section4Img/lego13.png" alt="레고 세트 13" />
-                            <img src="/section4Img/lego14.png" alt="레고 세트 14" />
-                            <img src="/section4Img/lego15.png" alt="레고 세트 15" />
+                            <img className="fall piece-9" src="/section4Img/lego9.png" alt="레고 세트 9" />
+                            <img className="fall piece-10" src="/section4Img/lego10.png" alt="레고 세트 10" />
+                            <img className="fall piece-11" src="/section4Img/lego11.png" alt="레고 세트 11" />
+                            <img className="fall piece-12" src="/section4Img/lego12.png" alt="레고 세트 12" />
+                            <img className="fall piece-13" src="/section4Img/lego13.png" alt="레고 세트 13" />
+                            <img className="fall piece-14" src="/section4Img/lego14.png" alt="레고 세트 14" />
+                            <img className="fall piece-15" src="/section4Img/lego15.png" alt="레고 세트 15" />
 
                         </div>
                     </div>
